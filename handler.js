919eef0b24aa9e7f47df6decc9abb9097228f3df
Joe Robles <joe.robles.pdj@gmail.com>
2019-09-05 00:01:27 -0500

    feature/basic crud for publication table

--- handler.js
+++ handler.js
@@ -199,3 +199,196 @@ module.exports.deleteAuthor = async (event, context, callback) => {
     );
   }
 };
+
+module.exports.createPublication = async (event, context, callback) => {
+  if (event && event.body) {
+    const { title, body, publicationDate, authors } = JSON.parse(event.body);
+    if (
+        !title ||
+        title.trim() === '' ||
+        !body ||
+        body.trim() === '' ||
+        !publicationDate ||
+        publicationDate.trim() === '' ||
+        !authors ||
+        authors.length() === 0
+    ) {
+      return callback(
+          null,
+          response(400, {
+            error: 'Put must have a title, a body, a publicationDate and authors, and they must not be empty'
+          })
+      );
+    }
+
+    const Item = {
+      publicationId: uniqid(),
+      title,
+      body,
+      publicationDate,
+      authors,
+      createdAt: new Date().toISOString(),
+    };
+    const params = {
+      TableName: PublicationTableName,
+      Item
+    };
+
+    return db.put(params)
+        .promise()
+        .then(() => {
+          callback(null, response(201, Item));
+        })
+        .catch(err => response(null, response(err.statusCode, err)));
+  } else {
+    return callback(
+        null,
+        response(400, {
+          error: 'Put must have a non empty body'
+        })
+    );
+  }
+};
+
+module.exports.getAllPublications = async (event, context, callback) => {
+  const params = {
+    TableName: PublicationTableName
+  };
+  return db.scan(params)
+      .promise()
+      .then(res => {
+        callback(null, response(200, res.Items))
+      })
+      .catch(err => callback(null, response(err.statusCode, err)));
+};
+
+module.exports.getPublicationById = async (event, context, callback) => {
+  if (event && event.pathParameters) {
+    const { publicationId } = event.pathParameters;
+
+    if (
+        !publicationId ||
+        publicationId.trim() === ''
+    ) {
+      return callback(
+          null,
+          response(400, {
+            error: 'Get must have a publicationId, and must not be empty'
+          })
+      );
+    }
+    const params = {
+      TableName: PublicationTableName,
+      Key: {
+        publicationId
+      }
+    };
+
+    return db.get(params)
+        .promise()
+        .then(res => {
+          callback(null, response(200, res.Item))
+        })
+        .catch(err => callback(null, response(err.statusCode, err)));
+  } else {
+    return callback(
+        null,
+        response(400, {
+          error: 'Get must have a non empty publicationId parameter'
+        })
+    );
+  }
+};
+
+module.exports.updatePublication = async (event, context, callback) => {
+  if (event && event.pathParameters && event.body) {
+    const { publicationId } = event.pathParameters,
+        { Item } = JSON.parse(event.body);
+
+    if (
+        !publicationId ||
+        publicationId.trim() === ''
+    ) {
+      return callback(
+          null,
+          response(400, {
+            error: 'Update must have a publicationId, and must not be empty'
+          })
+      );
+    }
+    let UpdateExpression = [], ExpressionAttributeValues = [];
+    Object.keys(Item).forEach(k => {
+      if (k !== 'updatedAt') {
+        UpdateExpression.push(`${k} = :${k}`);
+        ExpressionAttributeValues[`:${k}`] = Item[k];
+      }
+    });
+    UpdateExpression.push('updatedAt = :updatedAt');
+    ExpressionAttributeValues[':updatedAt'] = new Date().toISOString();
+
+    ExpressionAttributeValues[':publicationId'] = publicationId;
+
+    const params = {
+      TableName: PublicationTableName,
+      Key: {
+        publicationId
+      },
+      ConditionExpression: 'publicationId = :publicationId',
+      UpdateExpression: 'set ' + UpdateExpression.join(', '),
+      ExpressionAttributeValues,
+      ReturnValues: 'UPDATED_NEW',
+    };
+
+    return db.update(params)
+        .promise()
+        .then(res => {
+          callback(null, response(200, res.Attributes))
+        })
+        .catch(err => callback(null, response(err.statusCode, err)));
+  } else {
+    return callback(
+        null,
+        response(400, {
+          error: 'Update must have a non empty publicationId parameter'
+        })
+    );
+  }
+};
+
+module.exports.deletePublication = async (event, context, callback) => {
+  if (event && event.pathParameters) {
+    const { publicationId } = event.pathParameters;
+
+    if (
+        !publicationId ||
+        publicationId.trim() === ''
+    ) {
+      return callback(
+          null,
+          response(400, {
+            error: 'Delete must have a publicationId, and must not be empty'
+          })
+      );
+    }
+    const params = {
+      TableName: PublicationTableName,
+      Key: {
+        publicationId
+      }
+    };
+
+    return db.delete(params)
+        .promise()
+        .then(res => {
+          callback(null, response(200, { message: 'Publication deleted successfully' }))
+        })
+        .catch(err => callback(null, response(err.statusCode, err)));
+  } else {
+    return callback(
+        null,
+        response(400, {
+          error: 'Delete must have a non empty publicationId parameter'
+        })
+    );
+  }
+};
